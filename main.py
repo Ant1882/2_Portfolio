4a08ece4960b186b3055e3adf426a427c5adda5e
Ant1882 <antmarshall1882@gmail.com>
2022-12-19 15:37:10 +0000

    Add description on the new columns

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.png", width=300)
 
 with col2:
     st.title("Anthony Marshall")
@@ -14,3 +14,6 @@ with col2:
     from the University of Central Lancashire.
     """
     st.info(content)
+
+content2 = """Below you can find some of the apps I have built in Python."""
+st.write(content2)
