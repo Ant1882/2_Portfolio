71c1d0b2c9351de127f796e96d6c7fbbf0050a9c
Ant1882 <antmarshall1882@gmail.com>
2022-12-19 20:37:06 +0000

    Added description, images, links

--- main.py
+++ main.py
@@ -26,7 +26,9 @@ df = pandas.read_csv("data.csv", sep=";")
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
